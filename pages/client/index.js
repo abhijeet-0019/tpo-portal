4c2e0e5459499cd0dba6563d7c27d0a2071a471e
abhijeet <abhijeetsinghbhainser@gmail.com>
2023-04-10 23:20:13 +0530

    updated according to ui

--- pages/client/index.js
+++ pages/client/index.js
@@ -1,33 +1,43 @@
-import React from "react";
+import React from 'react'
+import Image from "next/image";
+import style from "./index.module.css";
+import profile from "../../public/assets/Profile.png";
 
-import SideNavbar from "../components/SideNavbar";
-import TopNavbar from "../components/TopNavbar"
 
-const Structure = () => {
+function Client() {
+    const list = ['Dashboard', 'Feeds', 'Companies', 'New User', 'Calender', 'Settings', 'FAQ', 'LogOut'];
+    const routes = [''];
     return (
-        <div className="wrapper">
-            <TopNavbar />
-            <div className="main_body">
-                <SideNavbar />
-                <div className="container">
-                    <div className="item_wrap">
-                        <div className="item item1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum omnis nihil aut aperiam adipisci suscipit ullam sunt saepe cupiditate quam distinctio officiis tempore laudantium, animi amet corrupti ratione est commodi! Sunt tempora quod magnam optio, reiciendis veritatis, necessitatibus eos molestias facilis reprehenderit maiores ipsum quaerat placeat laborum, a aspernatur corporis.</div>
-                        <div className="item item2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum omnis nihil aut aperiam adipisci suscipit ullam sunt saepe cupiditate quam distinctio officiis tempore laudantium, animi amet corrupti ratione est commodi! Sunt tempora quod magnam optio, reiciendis veritatis, necessitatibus eos molestias facilis reprehenderit maiores ipsum quaerat placeat laborum, a aspernatur corporis.
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum omnis nihil aut aperiam adipisci suscipit ullam sunt saepe cupiditate quam distinctio officiis tempore laudantium, animi amet corrupti ratione est commodi! Sunt tempora quod magnam optio, reiciendis veritatis, necessitatibus eos molestias facilis reprehenderit maiores ipsum quaerat placeat laborum, a aspernatur corporis.
-                        </div>
-                    </div>
+        <div className={style.parent}>
+            <div className={style.sidebar}>
+                <div className={style.inner_sidebar}>
+                    <ul>
+                        <li>
+                            <div className={style.list_tab}>
+                                <span className={style.list_tab}>
+                                    <div className={style.logo}>
+                                        <Image src={profile} className={style.image} />
+                                    </div>
+                                </span>
+                            </div>
+                        </li>
+                        {list.map((item) => (
+                            <li>
+                                <a href="#">
+                                    <div className={style.list_tab}>
+                                        <span className={style.list}>{item}</span>
+                                    </div>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </div>
+            <div className={style.content}>
+            components goes here
+            </div>
         </div>
-    );
-};
+    )
+}
 
-const Client = () => {
-    return (
-        <div>
-            <Structure />
-        </div>
-    );
-};
-
-export default Client;
+export default Client
