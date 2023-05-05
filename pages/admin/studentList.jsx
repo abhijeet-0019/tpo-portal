65df5e9ebf350b406165827ec51beed2cc39608d
sakshi <vashishthasakshi2@gmail.com>
2023-05-05 19:42:43 +0530

    added apiConfig

--- pages/admin/studentList.jsx
+++ pages/admin/studentList.jsx
@@ -1,8 +1,62 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { makeStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+})
+
+
 function StudentList() {
+
+    const classes = useStyles()
+  const [students, setStudents] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/placed_students')
+          .then(response => {
+            setStudents(response.data)
+            console.log(students)
+          })
+          .catch(error => {
+            console.error(error)
+          })
+      }, [])
+
     return ( 
-        <div>
-            <h1>hiiiiiiii</h1>
-        </div>
+        <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="placed students table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Company Name</TableCell>
+              <TableCell align="right">Branch</TableCell>
+              <TableCell align="right">Salary per Annum</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {students.map((student) => (
+              <TableRow key={student.id}>
+                <TableCell component="th" scope="row">
+                  {student.name}
+                </TableCell>
+                <TableCell align="right">{student.company_name}</TableCell>
+                <TableCell align="right">{student.branch}</TableCell>
+                <TableCell align="right">{student.salary_per_annum}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
      );
 }
 
--- pages/mainlogin/login.jsx
+++ pages/mainlogin/login.jsx
@@ -1,21 +1,38 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 import style from "./login.module.css";
+import useAPIAuth from "../../apiConfig_TPO/useAPIAuth";
+import useAPIData from "../../apiConfig_TPO/useAPIData";
 
 function Login() {
-  const [username, setUsername] = useState("");
+  const { setUser } = useAPIAuth();
+  const { getItems } = useAPIData();
+  const { getUserEmail } = useAPIAuth();
+
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
+    setEmail(event.target.value);
   };
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(`Username: ${username}, Password: ${password}`);
+  //console.log(email);
+  //console.log(password);
+  const handleLogin = async (event) => {
+    const user = { email, password };
+    const success = await setUser(user);
+   console.log(user);
+    if (success) {
+      router.push("/");
+    } else {
+      alert("Incorrect Login credentials");
+    }
   };
 
   return (
@@ -24,29 +41,31 @@ function Login() {
         <h1>Training and placement office</h1>
         <h2>MBM University</h2>
       </div>
-      <form onSubmit={handleSubmit} className={style.login }>
+      <form onSubmit={handleLogin} className={style.login}>
         <div className={style.userinput}>
-        <input
-          className={style.input}
-          type="text"
-          id="username"
-          value={username}
-          onChange={handleUsernameChange}
-          placeholder="Username"
-        />
-       
-        <input
-          className={style.input}
-          type="password"
-          id="password"
-          value={password}
-          onChange={handlePasswordChange}
-          placeholder="Password"
-        />
+          <input
+            className={style.input}
+            type="email"
+            id="username"
+            value={email}
+            onChange={handleUsernameChange}
+            placeholder="Username"
+          />
+
+          <input
+            className={style.input}
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            placeholder="Password"
+          />
         </div>
 
         <div className={style.submit}>
-        <button className={style.button} type="submit">Login</button>
+          <button className={style.button} type="submit">
+            Login
+          </button>
         </div>
       </form>
     </div>
